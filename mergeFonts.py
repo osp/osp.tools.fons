4fa265dfcce020706a7612e7732a554a6b7743cb
Stéphanie Vilayphiou <stephanie@stdin.fr>
2015-03-04 11:39:32 +0100

    script mergeFonts: combine several fonts (even more than 2) into one new font

---
+++ mergeFonts.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+"""
+Merges several font files into a new font (keeping the original fonts intact).
+It can take any font format that Fontforge can open: .ufo, .otf, .sfd...
+
+Usage:
+    python mergeFonts.py fonte1.ufo fonte2.ufo ...  fonte17.ufo fonte-out.ufo
+"""
+
+
+import fontforge
+import sys
+
+
+font1 = fontforge.open(sys.argv[1])
+font2 = sys.argv[2]
+args = len(sys.argv) - 1
+print args
+
+
+i = 0
+for arg in sys.argv:
+    print i
+    if i > 1 and i < args:
+        print arg
+        font1.mergeFonts(arg)
+    i += 1
+        
+
+
+font1.generate(sys.argv[-1])
